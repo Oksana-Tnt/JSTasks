dd5ddeb791e93ce6e5d3eebfed1ebd8360d32182
Oksana-Tnt <127055817+Oksana-Tnt@users.noreply.github.com>
2023-06-28 15:05:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,24 @@ function pigIt(str){
 
 console.log(pigIt('Hello world !'));
 
+
+
+function getDoubleNum(array, target) {   
+const resArray = [];
+
+  array.reduce((acc, item) => {
+       acc.hasOwnProperty(item) ? (acc[item] += 1) : (acc[item] = 1);
+       if (acc[item] <= target) {
+           resArray.push(item);
+       }
+       return acc;
+       
+    }  
+       , {});
+   
+    return resArray;
+       
+}
+
+console.log(getDoubleNum([1, 2, 3, 5, 2, 5, 1, 2, 5, 2, 4, 2, 4], 2));
+
