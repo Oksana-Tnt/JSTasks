03fb70320d6c4bcc7547cdf3e913ab9d75ed0463
Oksana-Tnt <oksanatantsiura@gmail.com>
2023-06-28 15:01:56 +0200

    task 2

--- script.js
+++ script.js
@@ -1,22 +1,15 @@
-
-function getDoubleNum(array, target) {   
-const resArray = [];
-
-  array.reduce((acc, item) => {
-       acc.hasOwnProperty(item) ? (acc[item] += 1) : (acc[item] = 1);
-       if (acc[item] <= target) {
-           resArray.push(item);
-       }
-       return acc;
-       
-    }  
-       , {});
+function pigIt(str){
+    const strArray = str.split(" ");
+    const res = [];
    
-    return resArray;
-       
+    for (let i = 0; i < strArray.length; i += 1){
+        
+        res.push(strArray[i].slice(1) + strArray[i].slice(0, 1) + "ay")
+     
+    }
+   
+    return res;
 }
 
-console.log(getDoubleNum([1, 2, 3, 5, 2, 5, 1, 2, 5, 2, 4, 2, 4], 2));
-
-
+console.log(pigIt('Hello world !'));
 
